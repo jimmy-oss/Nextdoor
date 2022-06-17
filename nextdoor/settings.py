665c20a02dde2d3677352eaf8145f2ba12831367
jimmy-oss <jimmynjonge51@gmail.com>
2022-06-17 15:48:26 +0300

    styling background

--- nextdoor/settings.py
+++ nextdoor/settings.py
@@ -71,10 +71,12 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            'libraries' : {
+                'staticfiles': 'django.templatetags.static', 
+            }
         },
     },
 ]
-
 WSGI_APPLICATION = 'nextdoor.wsgi.application'
 
 
