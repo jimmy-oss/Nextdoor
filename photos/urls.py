aad006bd4da1f5c4647da4d1cc6185da47fad90a
jimmy-oss <jimmynjonge51@gmail.com>
2022-06-19 01:48:17 +0300

    implemented search

--- photos/urls.py
+++ photos/urls.py
@@ -8,17 +8,11 @@ urlpatterns = [
     path('photo/<str:pk>/',views.viewPhoto, name='photo'),
     path('add',views.addPhoto, name='add'),
     path('follow', views.follow, name='follow'),
+    path('search', views.search, name='search'),
     path('signup',views.signup, name='signup'),
     path('signin',views.signin, name='signin'),
     path('settings',views.settings, name='settings'),
     path('profile/<str:pk>',views.profile, name='profile'),
     path('logout',views.logout, name='logout'),
-  
-   
-    
-   # path('search', views.search, name='search'),
- 
-  
- 
   
 ]
